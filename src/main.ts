17a4ddc911fdaa3ecf819ac35d704c4f50ce1949
MarvelMcD <MarvelMcD@gmail.com>
2024-12-01 22:27:54 -0800

    step 2 simple marker drawing
    css and mouse observer for basic clearing and drawing

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,40 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.id = "appCanvas";
 app.appendChild(canvas);
+
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear";
+clearButton.id = "clearButton";
+app.appendChild(clearButton);
+
+const ctx = canvas.getContext("2d")!;
+ctx.lineWidth = 2;
+ctx.lineCap = "round";
+ctx.strokeStyle = "black";
+
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", () => {
+  isDrawing = true;
+  ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", (event) => {
+  if (!isDrawing) return;
+
+  const rect = canvas.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  ctx.closePath();
+});
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
